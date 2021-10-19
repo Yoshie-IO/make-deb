0d0f26898d2515d79c911a936ef040f44a1b1a84
BrunoWidmann <bruno.widmann@yoshie.io>
2021-10-19 14:38:51 +0200

    example setup.py
    make-debian-dir reads in and uses the following
    fields from a projects setup.py:
     * name
     * version
     * maintainer
     * maintainer_email
     * description
    
    Put it in a python-project dir where make-debian-dir
    should be invoked

---
+++ examples/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='example-name',
+    version='0.0.5',
+    maintainer = "Example Maintainer",
+    maintainer_email = "example@example.nil",
+    description = "Example description",
+)
